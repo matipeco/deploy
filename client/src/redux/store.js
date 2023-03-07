a4720f011acb1be8feea476324d2e73f0420d1f3
matipeco <matiaspecorale1995@gmail.com>
2023-03-07 20:20:37 -0300

    update

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,8 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { reducer } from "./reducer";
 import thunk from "redux-thunk";
 
 export const store = createStore(
-    reducer,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    reducer, applyMiddleware(thunk)
+
 );
